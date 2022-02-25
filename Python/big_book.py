91cea3bc3a34cb7616b6174b5343a76e31a1e94c
FERNANDO <elderarias2015@gmail.com>
2022-02-25 18:03:59 -0300

    FOUR IN A ROW

--- Python/big_book.py
+++ Python/big_book.py
@@ -1,91 +1,136 @@
-from ast import Return
-import random
+import re
 import sys
-import time
+from traceback import print_tb
+from urllib import response
 
-try:
-    import bext
-except ImportError:
-    print('This program requires the bext module, which you')
-    print('can install by following the instructions at')
-    print('https://pypi.org/project/Bext/')
-    sys.exit()
+EMPTY_SPACE='.'
+PLAYER_X='X'
+PLAYER_O='O'
 
-WIDTH = 79
-HEIGHT = 22
-
-TREE = 'A'
-FIRE = 'W'
-EMPTY = ' '
-
-INITIAL_TREE_DENSITY = 0.5
-GROW_CHANCE = 0.01
-FIRE_CHANCE = 0.09
-
-PAUSE_LENGTH = 0.2
-
+BOARD_WIDTH=7
+BOARD_HEIGHT=6
+COLUMN_LABELS=('1','2','3','4','5','6','7')
+assert len(COLUMN_LABELS)==BOARD_WIDTH
 
 def main():
-    forest = create_new_forest()
-    bext.clear()
+    print("""Four in a Row, by Al Sweigart al@inventwithpython.com
+
+Two players take turns dropping tiles into one of seven columns, trying
+to make four in a row horizontally, vertically, or diagonally.
+""")
+
+    game_board=get_new_board()
+    player_turn=PLAYER_X
 
     while True:
-        display_forest(forest)
+        display_board(game_board)
+        player_move=ask_for_player_move(player_turn, game_board)
+        game_board[player_move]=player_turn
 
-        next_forest = {'width': forest['width'], 'height': forest['height']}
-        for x in range(forest['width']):
-            for y in range(forest['height']):
-                if (x, y) in next_forest:
-                    continue
+        if is_winner(player_turn, game_board):
+            display_board(game_board)
+            print(f'Player {player_turn} has won!')
+            sys.exit()
+        elif is_full(game_board):
+            display_board(game_board)
+            print('There is a tie!')
+            sys.exit()
+        
+        if player_turn==PLAYER_X:
+            player_turn=PLAYER_O
+        elif player_turn==PLAYER_O:
+            player_turn=PLAYER_X
 
-                if((forest[(x, y)] == EMPTY) and (random.random() <= GROW_CHANCE)):
-                    next_forest[(x, y)] = TREE
-                elif((forest[(x, y)] == TREE) and (random.random() <= FIRE_CHANCE)):
-                    next_forest[(x, y)] = FIRE
-                elif forest[(x,y)]==FIRE:
-                    for ix in range(-1,2):
-                        for iy in range(-1,2):
-                            if forest.get((x+ix,y+iy))==TREE:
-                                next_forest[(x+ix,y+iy)]=FIRE
-                    next_forest[(x,y)]=EMPTY
-                else:
-                    next_forest[(x,y)]=forest[(x,y)]
-        forest=next_forest
+def get_new_board():
+    board={}
+    for column_index in range(BOARD_WIDTH):
+        for row_index in range(BOARD_HEIGHT):
+            board[(column_index,row_index)]=EMPTY_SPACE
+    return board
 
-        time.sleep(PAUSE_LENGTH)
+def display_board(board):
+    tile_chars=[]
+    for row_index in range(BOARD_HEIGHT):
+        for column_index in range(BOARD_WIDTH):
+            tile_chars.append(board[(column_index, row_index)])
+    
+    print('''
+     1234567 
+    +-------+
+    |{}{}{}{}{}{}{}|
+    |{}{}{}{}{}{}{}|
+    |{}{}{}{}{}{}{}|
+    |{}{}{}{}{}{}{}|
+    |{}{}{}{}{}{}{}|
+    |{}{}{}{}{}{}{}|
+    +-------+
+    '''.format(*tile_chars))
+
+def ask_for_player_move(player_tile, board):
+    while True:
+        print(f'Player {player_tile}, enter a column or QUIT: ')
+        response=input('> ').upper().strip()
+
+        if response=='QUIT':
+            print('Thanks for playing!')
+            sys.exit()
+        
+        if response not in COLUMN_LABELS:
+            print(f'Enter a number for 1 to {BOARD_WIDTH}')
+            continue
+
+        column_index=int(response) - 1
+
+        if board[(column_index,0)] != EMPTY_SPACE:
+            print('That column is full, select another one.')
+            continue
+
+        for row_index in range(BOARD_HEIGHT-1, -1, -1):
+            if board[(column_index, row_index)]==EMPTY_SPACE:
+                return(column_index, row_index)
+
+def is_full(board):
+    for row_index in range(BOARD_HEIGHT):
+        for column_index in range(BOARD_WIDTH):
+            if board[(column_index, row_index)]==EMPTY_SPACE:
+                return False
+    return True
 
 
-def create_new_forest():
-    forest = {'width': WIDTH, 'height': HEIGHT}
-    for x in range(WIDTH):
-        for y in range(HEIGHT):
-            if(random.random()*100) <= INITIAL_TREE_DENSITY:
-                forest[(x, y)] = TREE
-            else:
-                forest[(x, y)] = EMPTY
-    return forest
+def is_winner(player_tile, board):
+    for column_index in range(BOARD_WIDTH - 3):
+        for row_index in range(BOARD_HEIGHT):
+            tile1=board[(column_index, row_index)]
+            tile2=board[(column_index+1, row_index)]
+            tile3=board[(column_index+2, row_index)]
+            tile4=board[(column_index+3, row_index)]
+            if tile1==tile2==tile3==tile4==player_tile:
+                return True
 
+    for column_index in range(BOARD_WIDTH):
+        for row_index in range(BOARD_HEIGHT-3):
+            tile1=board[(column_index, row_index)]
+            tile2=board[(column_index, row_index+1)]
+            tile3=board[(column_index, row_index+2)]
+            tile4=board[(column_index, row_index+3)]
+            if tile1==tile2==tile3==tile4==player_tile:
+                return True
 
-def display_forest(forest):
-    bext.goto(0, 0)
-    for y in range(forest['height']):
-        for x in range(forest['width']):
-            if forest[(x, y)] == TREE:
-                bext.fg('green')
-                print(TREE, end='')
-            elif forest[(x, y)] == FIRE:
-                bext.fg('red')
-                print(FIRE, end='')
-            elif forest[(x, y)] == EMPTY:
-                print(EMPTY, end='')
-        print()
-    bext.fg('reset')
-    print(f'Grow chance: {GROW_CHANCE*100}%', end='')
-    print(f'Lightning chance: {FIRE_CHANCE*100}%', end='')
-    print('Press Ctrl-C to quit.')
+    for column_index in range(BOARD_WIDTH-3):
+        for row_index in range(BOARD_HEIGHT-3):
+            tile1=board[(column_index, row_index)]
+            tile2=board[(column_index+1, row_index+1)]
+            tile3=board[(column_index+2, row_index+2)]
+            tile4=board[(column_index+3, row_index+3)]
+            if tile1==tile2==tile3==tile4==player_tile:
+                return True
+            tile1=board[(column_index+3, row_index)]
+            tile2=board[(column_index+2, row_index+1)]
+            tile3=board[(column_index+1, row_index+2)]
+            tile4=board[(column_index, row_index+3)]
+            if tile1==tile2==tile3==tile4==player_tile:
+                return True
+    return False
 
 if __name__=='__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        sys.exit()
+    main()
