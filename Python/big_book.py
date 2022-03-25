8365e3f3b873c6ca726d60a804f81eb392a19b84
FERNANDO <elderarias2015@gmail.com>
2022-03-25 13:44:29 -0300

    big book robots-boring csv

--- Python/big_book.py
+++ Python/big_book.py
@@ -41,6 +41,13 @@ def main():
             sys.exit()
 
         player_position = ask_for_player_move(board, robots, player_position)
+        robots = move_robots(board, robots, player_position)
+
+        for x, y in robots:
+            if (x, y) == player_position:
+                display_board(board, robots, player_position)
+                print('You have been caught by a robot!')
+                sys.exit()
 
 
 def get_new_board():
@@ -94,7 +101,7 @@ def display_board(board, robots, player_position):
                 print(WALL, end='')
             elif board[(x, y)] == DEAD_ROBOT:
                 print(DEAD_ROBOT, end='')
-            elif (x, y) in player_position:
+            elif (x, y) == player_position:
                 print(PLAYER, end='')
             elif (x, y) in robots:
                 print(ROBOT, end='')
@@ -141,3 +148,53 @@ def ask_for_player_move(board, robots, player_position):
                 'A': (player_x-1, player_y+0),
                 'S': (player_x, player_y)
             }[move]
+
+
+def move_robots(board, robot_positions, player_position):
+    player_x, player_y = player_position
+    next_robot_positions = []
+
+    while len(robot_positions) > 0:
+        robot_x, robot_y = robot_positions[0]
+
+        if robot_x < player_x:
+            move_x = 1
+        elif robot_x > player_x:
+            move_x = -1
+        elif robot_x == player_x:
+            move_x = 0
+
+        if robot_y < player_y:
+            move_y = 1
+        elif robot_y > player_y:
+            move_y = -1
+        elif robot_y == player_y:
+            move_y = 0
+
+        if board[(robot_x+move_x, robot_y+move_y)] == WALL:
+            if board[(robot_x+move_x, robot_y)] == EMPTY_SPACE:
+                move_y = 0
+            elif board[(robot_x, robot_y+move_y)] == EMPTY_SPACE:
+                move_x = 0
+            else:
+                move_x = 0
+                move_y = 0
+        new_robot_x = robot_x+move_x
+        new_robot_y = robot_y+move_y
+
+        if (board[(robot_x, robot_y)] == DEAD_ROBOT or board[(new_robot_x, new_robot_y)] == DEAD_ROBOT):
+            del robot_positions[0]
+            continue
+
+        if (new_robot_x, new_robot_y) in next_robot_positions:
+            board[(new_robot_x, new_robot_y)] = DEAD_ROBOT
+            next_robot_positions.remove((new_robot_x, new_robot_y))
+        else:
+            next_robot_positions.append((new_robot_x, new_robot_y))
+
+        del robot_positions[0]
+    return next_robot_positions
+
+
+if __name__ == '__main__':
+    main()
