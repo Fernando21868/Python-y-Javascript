2470d59035e4a4338d36a75e0dc97a5d491f0d95
FERNANDO <elderarias2015@gmail.com>
2022-03-31 18:13:10 -0300

    20 projects vanilla 1-bootstrap-boring

--- Python/boring.py
+++ Python/boring.py
@@ -1,44 +1,11 @@
-import random
+import time
 
-try:
-    import pyperclip
-except ImportError:
-    pass
+print('Press Enter TO BEGIN. Afterward press ENTER to "click" the stopwatch. Press Ctrl-C to quit')
+input()
+print('Started')
 
+start_time = time.time()
+last_time = start_time
+lap_num = 1
 
-def main():
-    print('''L3375P34]< (leetspeek)
-By Al Sweigart al@inventwithpython.com
-
-Enter your leet message:''')
-    english = input('> ')
-    print()
-    leetspeak = englishToLeetspeak(english)
-    print(leetspeak)
-
-    try:
-        pyperclip.copy(leetspeak)
-        print('(Copied leetspeak to clipboard.)')
-    except NameError:
-        pass
-
-
-def englishToLeetspeak(message):
-    char_mapping = {
-        'a': ['4', '@', '/-\\'], 'c': ['('], 'd': ['|)'], 'e': ['3'],
-        'f': ['ph'], 'h': [']-[', '|-|'], 'i': ['1', '!', '|'], 'k': [']<'],
-        'o': ['0'], 's': ['$', '5'], 't': ['7', '+'], 'u': ['|_|'],
-        'v': ['\\/']}
-    leetspeak = ''
-    for char in message:
-        if char.lower() in char_mapping and random.random() <= 0.70:
-            posibble_replacements = char_mapping[char.lower()]
-            leet_replacement = random.choice(posibble_replacements)
-            leetspeak += leetspeak+leet_replacement
-        else:
-            leetspeak = leetspeak+char
-    return leetspeak
-
-
-if __name__ == '__main__':
-    main()
+# TODO: Start tracking the lap times.
