752060f25e56c88dfa2145ecef5bb255efc7e892
FERNANDO <elderarias2015@gmail.com>
2022-05-16 13:07:53 -0300

    asdf

--- Javascript, HTML, CSS/20 projects vanilla/form validator/script.js
+++ Javascript, HTML, CSS/20 projects vanilla/form validator/script.js
@@ -1,88 +1,24 @@
-const main = document.getElementById('main');
-const addUserBtn = document.getElementById('add-user');
-const doubleBtn = document.getElementById('double');
-const showMillionairesBtn = document.getElementById('show-millionaires');
-const sortBtn = document.getElementById('sort');
-const calculateWealthBtn = document.getElementById('calculate-wealth');
+const toggle = document.getElementById('toggle');
+const close = document.getElementById('close');
+const open = document.getElementById('open');
+const modal = document.getElementById('modal');
 
-let data = [];
+// Toglle nav
+toggle.addEventListener('click', () => {
+  document.body.classList.toggle('show-nav');
+});
 
-getRandomUser();
-getRandomUser();
-getRandomUser();
+// Show modal
+open.addEventListener('click', () => {
+  modal.classList.add('show-modal');
+});
 
-// Fetch random user and add money
-async function getRandomUser() {
-  const res = await fetch('https://randomuser.me/api');
-  const data = await res.json();
-  const user = data.results[0];
-  const newUser = {
-    name: `${user.name.first} ${user.name.last}`,
-    money: Math.floor(Math.random() * 1_000_000),
-  };
-  addData(newUser);
-}
+// Hide modal
+close.addEventListener('click', () => {
+  modal.classList.remove('show-modal');
+});
 
-// Double everyones money
-function doubleMoney() {
-  data = data.map((user) => {
-    return { ...user, money: user.money * 2 };
-  });
-  updateDom();
-}
-
-// Sort users by richest
-function sortByRichest() {
-  data.sort((a, b) => b.money - a.money);
-  updateDom();
-}
-
-// Filter only millionaires
-function showMillionaires() {
-  data = data.filter((user) => user.money >= 1_000_000);
-  updateDom();
-}
-
-// Calculate the total wealth
-function calculateWealth() {
-  const wealth = data.reduce((acc, user) => (acc += user.money), 0);
-
-  const wealthEl = document.createElement('div');
-  wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(
-    wealth
-  )}</strong></h3>`;
-  main.appendChild(wealthEl);
-}
-
-// Add new obj to data arr
-function addData(obj) {
-  data.push(obj);
-  updateDom();
-}
-
-// Update DOM
-function updateDom(providedData = data) {
-  // Clear main div
-  main.innerHTML = `<h2><strong>Person</strong>Wealth</h2>`;
-
-  providedData.forEach((item) => {
-    const element = document.createElement('div');
-    element.classList.add('person');
-    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
-      item.money
-    )}`;
-    main.appendChild(element);
-  });
-}
-
-// Form number as money
-function formatMoney(number) {
-  return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
-}
-
-// Event listeners
-addUserBtn.addEventListener('click', getRandomUser);
-doubleBtn.addEventListener('click', doubleMoney);
-sortBtn.addEventListener('click', sortByRichest);
-showMillionairesBtn.addEventListener('click', showMillionaires);
-calculateWealthBtn.addEventListener('click', calculateWealth);
+// Hide modal on outside click
+window.addEventListener('click', (e) => {
+  e.target == modal ? modal.classList.remove('show-modal') : false;
+});
